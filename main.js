7661e016f1774f723f2e6dd23f9e71ab67afad17
Mark Withers <mark26266@gmail.com>
2014-09-08 18:08:42 +0100

    Set / to go to index.html

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(req, res){
-  res.send('Hello World');
+  res.sendfile(__dirname + '/public/index.html');
 });
 
 app.use(express.static(__dirname + '/public'))
